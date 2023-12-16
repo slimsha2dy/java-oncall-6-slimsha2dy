a491e0e70fad1ae8777cb770a03598b41342c8c2
최휘용 <chy2581@naver.com>
2023-12-16 16:33:44 +0900

    feat: 다음 순번의 근무자를 반환하는 WorkQueue의 메서드 구현
    매번 호출할 때마다 다음 근무자를 리턴하는 getNext(String name)메서드 구현
    인자로 받은 name과 현재 반환할 근무자가 겹치는 경우 내부적으로 순서를 바꿔서 반환하도록 구현

--- src/main/java/oncall/model/WorkQueue.java
+++ src/main/java/oncall/model/WorkQueue.java
@@ -8,6 +8,8 @@ import oncall.util.ErrorMessage;
 
 public class WorkQueue {
     List<String> nameQueue;
+    int temporaryIndex;
+    boolean isSwitchedFlag;
 
     public WorkQueue(List<String> input) {
         Validator.validateWorkQueue(input);
@@ -18,6 +20,8 @@ public class WorkQueue {
             }
             nameQueue.add(string);
         }
+        this.temporaryIndex = 0;
+        this.isSwitchedFlag = false;
     }
 
     public int size() {
@@ -31,5 +35,32 @@ public class WorkQueue {
             }
         }
         return true;
+    }
+
+    public String getNext(String name) {
+        // 다음 반환할 멤버가 name과 겹칠 경우 순서를 바꿔서 반환함
+        if (isSwitchedFlag) {
+            return switchedCase();
+        }
+        String result = nameQueue.get(temporaryIndex);
+        temporaryIndex = (temporaryIndex + 1) % nameQueue.size();
+        if (!result.equals(name)) {
+            return result;
+        }
+        result = nameQueue.get(temporaryIndex);
+        isSwitchedFlag = true;
+        return result;
+    }
+
+    public String switchedCase() {
+        // 이전에 연속이었던 경우, 이전 것을 반환한 후 다음 것을 반환하기 위해 Index를 조정
+        int prevIndex = temporaryIndex - 1;
+        if (prevIndex < 0) {
+            prevIndex += nameQueue.size();
+        }
+        String result = nameQueue.get(prevIndex);
+        temporaryIndex++;
+        isSwitchedFlag = false;
+        return result;
     }
 }
